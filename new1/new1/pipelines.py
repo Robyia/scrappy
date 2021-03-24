a903fb3e8924dde2e855e03d3da40d409423daa4
Robyia <59112000+Robyia@users.noreply.github.com>
2021-03-24 14:26:05 +0530

    Add files via upload

---
+++ new1/new1/pipelines.py
@@ -0,0 +1,43 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exporters import CsvItemExporter, JsonItemExporter
+
+
+class CsvPipeline(object):
+    def __init__(self):
+        self.file = open("csvFiles.csv", 'wb')
+        self.exporter = CsvItemExporter(self.file, unicode)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+
+def process_item(item, spider):
+    return item
+
+
+class New1Pipeline:
+    pass
+
+class JsonPipeline(object):
+    def __init__(self):
+        self.file = open("jsonFiles.json", 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
